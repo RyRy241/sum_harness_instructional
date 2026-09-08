f4b2c043e2d415b5717b1a9a6b824f734c44b14e
KoB <142365031+RyRy241@users.noreply.github.com>
2026-09-07 20:07:47 -0700

    Part 2

--- sum_vector.cpp
+++ sum_vector.cpp
@@ -11,14 +11,20 @@
 void 
 setup(int64_t N, int64_t A[])
 {
-   printf(" inside sum_vector problem_setup, N=%lld \n", N);
+   printf(" inside sum_vector problem_setup, N=%lld \n", (long long)N);
+
+   // fill the array with the values 0 .. N-1
+   for (int64_t i = 0; i < N; i++)
+      A[i] = i;
 }
 
 int64_t
 sum(int64_t N, int64_t A[])
 {
-   printf(" inside sum_vector perform_sum, N=%lld \n", N);
-
-   return 0;
+   // printf(" inside sum_vector perform_sum, N=%lld \n", N);
+   int64_t sum = 0;
+   for (int64_t i = 0; i < N; i++)
+      sum += A[i];
+   return sum;
 }
 
