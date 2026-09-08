42a3fa4f833274d03594210cf1b7bd0a657b6493
KoB <142365031+RyRy241@users.noreply.github.com>
2026-09-07 20:32:14 -0700

    Part 3

--- sum_indirect.cpp
+++ sum_indirect.cpp
@@ -5,20 +5,27 @@
 #include <random>
 #include <vector>
 #include <string.h>
+#include <stdlib.h>
 
 #include "sums.h"
 
 void 
 setup(int64_t N, int64_t A[])
 {
-   printf(" inside sum_indirect problem_setup, N=%lld \n", N);
+   printf(" inside sum_indirect problem_setup, N=%lld \n", (long long)N);
+
+   for (int64_t i = 0; i < N; i++)
+      A[i] = lrand48() % N;
 }
 
 int64_t
 sum(int64_t N, int64_t A[])
 {
-   printf(" inside sum_indirect perform_sum, N=%lld \n", N);
+   // printf(" inside sum_indirect perform_sum, N=%lld \n", (long long)N);
 
-   return 0;
+   int64_t sum = 0;
+   for (int64_t i = 0; i < N; i++)
+      sum += A[A[i]];
+   return sum;
 }
 
