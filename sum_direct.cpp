941915dd2f4598684556bf12dae323cf2be52a80
KoB <142365031+RyRy241@users.noreply.github.com>
2026-09-09 23:54:43 -0700

    pdf

--- sum_direct.cpp
+++ sum_direct.cpp
@@ -18,13 +18,11 @@ setup(int64_t N, int64_t A[])
 int64_t
 sum(int64_t N, int64_t A[])
 {
-   (void)A;
-
    int64_t sum = 0;
    for (int64_t i = 0; i < N; i++) {
       sum += i;
    }
-   
+
    return sum;
 }
 
